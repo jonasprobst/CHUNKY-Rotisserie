999ba2b1c71c25055f660e2c6246745ef617c03d
Jonas Probst <probst.jonas@gmail.com>
2023-07-10 22:43:26 +0200

    removes incomplete tests

--- test/test_unit/test_WifiAPConfigServer.cpp
+++
@@ -1,9 +0,0 @@
-#include "WifiAPConfigServer.h"
-#include "../mocks/mock_NVSSettingsInterface.h"
-#include <unity.h>
-
-// write unit tests for WifiAPConfigServer here
-void test_WifiAPConfigServer()
-{
-    TEST_ASSERT_EQUAL(1, 1);
-}
