7af0e49a1900da53defe33b7ec031d3136813458
Jonas Probst <probst.jonas@gmail.com>
2023-08-07 13:49:33 +0200

    tidies up DMX controller removing unrelated logic

--- lib/DMXController/DMXController.cpp
+++ lib/DMXController/DMXController.cpp
@@ -7,87 +7,76 @@ static const char *TAG = "DMXController";
 DMXController::DMXController(uint8_t base_channel)
     : base_channel_(base_channel), is_connected_(false)
 {
-  // Initialise DMX Connection
+  // Initialise DMX Connection and print status
   ESP_LOGI(TAG, "Port enable: %s", dmx_set_pin(dmx_port_, DMX_TX, DMX_RX, DMX_ENABLE));
   ESP_LOGI(TAG, "Driver install: %s", dmx_driver_install(dmx_port_, DMX_DEFAULT_INTR_FLAGS));
-}
-
-DMXController::~DMXController()
-{
-  // nothing to do here
 }
 
 uint16_t DMXController::GetTargetPosition()
 {
   // If an error occurred receiving the new position, the last position will be returned.
   // this is to prevent the motor from moving unintentionally towards a new position
-  position_hb_ = Read(POSITION_CHANNEL_HB, position_hb_);
-  position_lb_ = Read(POSITION_CHANNEL_LB, position_lb_);
-  return position_hb_ + (position_lb_ << 8);
+  position_hb_ = GetChannelValue(POSITION_CHANNEL_HB, position_hb_);
+  position_lb_ = GetChannelValue(POSITION_CHANNEL_LB, position_lb_);
+  target_position_ = position_hb_ + (position_lb_ << 8);
+  ESP_LOGI(TAG, "Target Position: %d", target_position_);
+  return target_position_;
 }
 
 uint8_t DMXController::GetMaxSpeed()
 {
-  // If an error occurred receiving the new max speed, 0 will be returned to stop the motor.
-  max_speed_ = Read(MAX_SPEED_CHANNEL);
+  // retruns 0 on error to stop the motor.
+  max_speed_ = GetChannelValue(MAX_SPEED_CHANNEL);
+  ESP_LOGI(TAG, "Max Speed: %d", max_speed_);
   return max_speed_;
 }
 
-uint8_t DMXController::GetMotorMode()
+uint8_t DMXController::GetCWSpeed()
 {
-  // If an error occurred receiving the new mode, the last mode will be returned.
+  // returns 0 on error to stop the motor
+  cw_speed_ = GetChannelValue(CW_SPEED_CHANNEL);
+  ESP_LOGI(TAG, "CW Speed: %d", cw_speed_);
+  return cw_speed_;
+}
+
+uint8_t DMXController::GetCCWSpeed()
+{
+  // returns 0 on error to stop the motor
+  ccw_speed_ = GetChannelValue(CCW_SPEED_CHANNEL);
+  ESP_LOGI(TAG, "CCW Speed: %d", ccw_speed_);
+  return ccw_speed_;
+}
+
+uint8_t DMXController::GetOperationMode()
+{
+  // Returns the last known operation mode on error.
   // this is to prevent the motor from moving unintentionally
-  motor_mode_ = Read(MOTOR_MODE_CHANNEL, motor_mode_);
-  return motor_mode_;
+  operation_mode_ = GetChannelValue(OPERATION_MODE_CHANNEL, operation_mode_);
+  ESP_LOGI(TAG, "Operation Mode: %d", operation_mode_);
+  return operation_mode_;
 }
 
-uint8_t DMXController::GetSpeed()
-{
-  uint8_t cw_speed = Read(CW_SPEED_CHANNEL);
-  uint8_t ccw_speed = Read(CCW_SPEED_CHANNEL);
-  if (cw_speed > 0 && ccw_speed > 0)
-  {
-    // Unallowed state. Stop motor.
-    return 0;
-  }
-  // set absolute value of speed
-  (ccw_speed > 0) ? speed_ = ccw_speed : speed_ = cw_speed;
-  ESP_LOGI(TAG, "Speed: %d", speed_);
-  return speed_;
-}
-
-uint8_t DMXController::GetDirection()
-{
-  uint8_t cw_speed = GetCWSpeed();
-  uint8_t ccw_speed = GetCCWSpeed();
-  if (cw_speed > 0 && ccw_speed > 0)
-  {
-    // Unallowed state. Stop motor.
-    return 0;
-  }
-  //TODO: return 1 or -1? or enum?
-  (ccw_speed > 0) ? direction_ = 1 : direction_ = 0; // 0 = CW, 1 = CCW
-  ESP_LOGI(TAG, "Direction: %d", direction_);
-  return direction_;
-}
-
-uint8_t DMXController::Read(uint8_t channel, uint8_t default_value)
+uint8_t DMXController::GetChannelValue(uint8_t channel, uint8_t default_value = 0)
 {
   // Check if DMX is connected and channel in range
-  if (!IsConnected() || channel < 1 || channel > NUM_CHANNELS)
+  // On error, return default value to prevent motor from moving unintentionally
+  // if default value is not set, 0 will be returned
+  if (!IsConnected())
   {
-    ESP_LOGE(TAG, "DMX Signal lost or invalid channel number: %d", channel);
-    // Return default value to prevent motor from moving unintentionally
-    // if default value is not set, 0 will be returned
+    ESP_LOGE(TAG, "Error: DMX not connected.");
+    return default_value;
+  }
+  else if (channel < 1 || channel > NUM_CHANNELS)
+  {
+    ESP_LOGE(TAG, "Error: Invalid channel number: %d", channel);
     return default_value;
   }
   // Return the value of the requested channel
-  return data_[channel - 1];
+  return channel_values_[channel - 1];
 }
 
 bool DMXController::IsConnected()
 {
-  ESP_LOGE(TAG, "IsConnected: %d", is_connected_);
   return is_connected_;
 }
 
@@ -97,7 +86,7 @@ bool DMXController::Update()
 
   // TODO: make this non-blocking if possible!
   // Try to receive a DMX package (this blocks until one is receive or it times out)
-  if (dmx_receive(dmx_port_, &packet, DMX_TIMEOUT_TICK) > 0) // TODO: does this work?
+  if (dmx_receive(dmx_port_, &packet, DMX_TIMEOUT_TICK) > 0)
   {
     is_connected_ = false;
     ESP_LOGE(TAG, "Timed out waiting for DMX packet. Error: %s", esp_err_to_name(packet.err));
@@ -111,25 +100,9 @@ bool DMXController::Update()
     ESP_LOGE(TAG, "Error receiving DMX packet: %s", esp_err_to_name(packet.err));
   }
 
-  // Package received succesfully. Read the data.
-  dmx_read(dmx_port_, data_, NUM_CHANNELS);
+  // Package received succesfully. Read the channel values.
+  dmx_read(dmx_port_, channel_values_, NUM_CHANNELS);
   is_connected_ = true;
   ESP_LOGI(TAG, "DMX packet received successfully.");
   return true;
-}
-
-uint8_t DMXController::GetCWSpeed()
-{
-  // returns 0 on error to stop the motor
-  uint8_t cw_speed = Read(CW_SPEED_CHANNEL);
-  ESP_LOGI(TAG, "CW Speed: %d", cw_speed);
-  return cw_speed;
-}
-
-uint8_t DMXController::GetCCWSpeed()
-{
-  // returns 0 on error to stop the motor
-  uint8_t ccw_speed = Read(CCW_SPEED_CHANNEL);
-  ESP_LOGI(TAG, "CCW Speed: %d", ccw_speed);
-  return ccw_speed;
 }
--- lib/DMXController/DMXController.h
+++ lib/DMXController/DMXController.h
@@ -2,9 +2,6 @@
 #define DMXCONTROLLER_H
 
 #include <esp_dmx.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/semphr.h>
 
 /**
  * @class DMXController
@@ -21,21 +18,17 @@ class DMXController
 public:
   /**
    * @brief Construct a new DMX Controller object.
+   * @note The base channel (aka offset adress) is set to 1.
    */
   DMXController();
 
   /**
    * @brief Construct a new DMX Controller object.
    *
-   * @param base_channel Set a base channel aka offset address to retrieve the channels from.
+   * @param base_channel Set a base channel (aka offset address) to retrieve the channels from.
    *                     eg. 200 to retriev channel 201, 202, etc.
    */
   DMXController(uint8_t base_channel);
-
-  /**
-   * @brief Destroy the DMX Controller object.
-   */
-  ~DMXController();
 
   /**
    * @brief Retrieve the motor mode from the DMX data.
@@ -43,7 +36,7 @@ public:
    *
    * @return The operation mode.
    */
-  uint8_t GetMotorMode();
+  uint8_t GetOperationMode();
 
   /**
    * @brief Retrieve the 16bit position value from the DMX data. (Channel 1+2 combined)
@@ -59,76 +52,6 @@ public:
    * @return The max speed (speed limit).
    */
   uint8_t GetMaxSpeed();
-
-  /**
-   * @brief Retrieve the absolute speed from the DMX data. The absolut speed
-   *        ranges from zero to max speed. Zero is stopped and 255 is
-   *        max speed. Zero is stopped.
-   *
-   * @return The absolute speed.
-   */
-  uint8_t GetSpeed();
-
-  /**
-   * @brief Retrieve the direction from the DMX data.
-   *
-   * @return The direction (0 = CW, 1 = CCW).
-   */
-  uint8_t GetDirection();
-
-/**
- * @brief: Retrieve the save limits flag from the DMX data. When in
- *         "Save limits"-Mode (Channel 6 set to 51-54%), the current Position
- *         of the motor should be saved as limit when Channel 4 or 5 is set to 0.
- * 
- * @param direction: 0 = CW, 1 = CCW
- * @return true if the flag is set, false otherwise.
- */
-
-  bool savePositionAsLimit(uint8_t direction);
-
-  /**
-   * @brief Retrieve the value of a single channel from the DMX data.
-   *
-   * @param channel The channel aka DMX address to read.
-   * @return The channel value or if there was an error, the default value.
-   */
-  uint8_t Read(uint8_t channel, uint8_t default_value = 0);
-
-  /**
-   * @brief Check if the DMX connection is active.
-   *
-   * @return true if the connection is active, false otherwise.
-   */
-  bool IsConnected();
-
-  /**
-   * @brief Update the DMX connection.
-   * @warning This function must be called regularly in the main loop.
-   */
-  bool Update();
-
-private:
-  // Channel settings
-  static constexpr uint8_t NUM_CHANNELS = 6;        // Number of consecutive channels to read.
-  static constexpr uint8_t POSITION_CHANNEL_HB = 1; // Position rough (High Byte of 16bit DMX Channel)
-  static constexpr uint8_t POSITION_CHANNEL_LB = 2; // Position fine (Low Byte of 16bit DMX Channel)
-  static constexpr uint8_t MAX_SPEED_CHANNEL = 3;   // Set the maximum speed
-  static constexpr uint8_t CW_SPEED_CHANNEL = 4;    // Rotate CW and set soft CW limit
-  static constexpr uint8_t CCW_SPEED_CHANNEL = 5;   // Rotate CCW and set soft CCW limit
-  static constexpr uint8_t MOTOR_MODE_CHANNEL = 6;  // Modus Operandi
-
-  dmx_port_t dmx_port_ = DMX_NUM_1; // The UART port to use. WARNING: Use the correct pin setup for this!
-  uint8_t base_channel_ = 0;        // The base channel (aka dmx address aka offset) to read from.
-  uint8_t data_[NUM_CHANNELS];      // The data array to store the read data in.
-  bool is_connected_;               // Flag to indicate if the DMX connection is active.
-
-  uint8_t position_hb_; // The high byte of the last received position
-  uint8_t position_lb_; // The low byte of the last received position
-  uint8_t motor_mode_;  // The last received motor mode
-  uint8_t max_speed_;   // The last received max speed
-  uint8_t speed_;            // The absolute speed
-  uint8_t direction_;        // THe direction the motor is turning in (CW/CCW)
 
   /**
    * @brief Retrieve the limit clock wise speed value from the DMX data.
@@ -149,6 +72,49 @@ private:
    * @return The counter clock wise speed value (% of max speed).
    */
   uint8_t GetCCWSpeed();
-};
 
+  /**
+   * @brief Retrieve the value of a single channel from the DMX data.
+   *
+   * @param channel The channel aka DMX address to read.
+   * @return The channel value or if there was an error, the default value.
+   */
+  uint8_t GetChannelValue(uint8_t channel, uint8_t default_value = 0);
+
+  /**
+   * @brief Check if the DMX connection is active.
+   *
+   * @return true if the connection is active, false otherwise.
+   */
+  bool IsConnected();
+
+  /**
+   * @brief Update the DMX connection.
+   * @warning This function must be called regularly in the main loop.
+   */
+  bool Update();
+
+private:
+  // Channel settings
+  static constexpr uint8_t NUM_CHANNELS = 6;           // Number of consecutive channels to read.
+  static constexpr uint8_t POSITION_CHANNEL_HB = 1;    // Position rough (High Byte of 16bit DMX Channel)
+  static constexpr uint8_t POSITION_CHANNEL_LB = 2;    // Position fine (Low Byte of 16bit DMX Channel)
+  static constexpr uint8_t MAX_SPEED_CHANNEL = 3;      // Set the maximum speed
+  static constexpr uint8_t CW_SPEED_CHANNEL = 4;       // Rotate CW and set soft CW limit
+  static constexpr uint8_t CCW_SPEED_CHANNEL = 5;      // Rotate CCW and set soft CCW limit
+  static constexpr uint8_t OPERATION_MODE_CHANNEL = 6; // Modus Operandi
+
+  dmx_port_t dmx_port_ = DMX_NUM_1; // The UART port to use. WARNING: Use the correct pin setup for this!
+  uint8_t base_channel_ = 0;        // The base channel (aka dmx address aka offset) to read from.
+  uint8_t channel_values_[NUM_CHANNELS];      // The array to store the read values in.
+  bool is_connected_;               // Flag to indicate if the DMX connection is active.
+
+  uint8_t position_hb_;      // The high byte of the last received position
+  uint8_t position_lb_;      // The low byte of the last received position
+  uint16_t target_position_; // The target position
+  uint8_t max_speed_;        // The last received max speed
+  uint8_t cw_speed_;         // The last received clock wise speed
+  uint8_t ccw_speed_;        // The last received counter clock wise speed
+  uint8_t operation_mode_;   // The operation mode
+};
 #endif // DMXCONTROLLER_H
