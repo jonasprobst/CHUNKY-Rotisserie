e225db0a71348ad282d7904bbdaf727175fbeffd
Jonas Probst <probst.jonas@gmail.com>
2023-11-22 14:54:56 +0100

    fixed faulty pinouts

--- lib/Config/PinConfig.h
+++ lib/Config/PinConfig.h
@@ -6,13 +6,24 @@
 
 // SparkFun ESP32 Thing Plus (https://www.sparkfun.com/products/20168)
 // [xx] refers to the pin used on the Thing/ DMX Shield
-constexpr gpio_num_t THING_SDA = GPIO_NUM_23; // I2C SDA [Pin 12]
-constexpr gpio_num_t THING_SCL = GPIO_NUM_23; // I2C SCL [Pin 11]
+// Used for communication with 
+// Display - SSD1306 128x32px I2C OLED Display
+// Use this library: https://github.com/olikraus/u8glib
+constexpr gpio_num_t THING_SDA = GPIO_NUM_21; // I2C SDA [Pin 12]
+constexpr gpio_num_t THING_SCL = GPIO_NUM_22; // I2C SCL [Pin 11]
 
 // SparkFun DMX-To-LED Shield (https://www.sparkfun.com/products/15110)
 constexpr gpio_num_t DMX_ENABLE = GPIO_NUM_4; // GPIO_NUM_21; // Driver Output Enable (DE) of MAX3485. High for TX. [Pin 16]
 constexpr gpio_num_t DMX_TX = GPIO_NUM_17;    // UART2 TX [Pin 14]
 constexpr gpio_num_t DMX_RX = GPIO_NUM_16;    // UART2 RX [Pin 15]
+
+// SparkFun ESP32 Thing Plus (USB-C) connected to SparkFun DMX-To-LED Shield
+// From LED Shield to ESCON, the following pins are used:
+constexpr gpio_num_t MOTOR_ENABLE = GPIO_NUM_27;    // DMX-Shield DATA3 - shifted to 5V
+constexpr gpio_num_t MOTOR_DIRECTION = GPIO_NUM_19; // DMX-Shield DATA2 - shifted to 5V
+constexpr gpio_num_t MOTOR_STEP = GPIO_NUM_23;      // DMX-Shield DATA1 - shifted to 5V
+constexpr gpio_num_t MOTOR_RUNNING = GPIO_NUM_18;    // DMX-Shield CLOCK - shifted to 5V
+
 
 // Maxon Servokontroller: ESCON 36/2 DC, 4-Q Servokontroller, 10-36 V, 2A/4A (https://www.maxongroup.ch/maxon/view/product/control/4-Q-Servokontroller/403112)
 // Maxon Motor: DCX 22S GB KL 24V with Planetary Gearhead GPX22 A 138:1 and Encoder ENC30 HEDL 5540 500IMP
@@ -22,36 +33,12 @@ constexpr gpio_num_t DMX_RX = GPIO_NUM_16;    // UART2 RX [Pin 15]
 // - D4 out -> Speed Comparator (high -> motor speed is > 0)
 // - P1 in -> Speed Ramp (Potentiometer on board 0 - 2000rpm. Set <=50% to start)
 // IMPORTANT: Currently the ramp is set on the potentiometer on the ESCON board
-constexpr gpio_num_t MOTOR_ENABLE = GPIO_NUM_27;    // DMX-Shield DATA2 - shifted to 5V - [Pin 6] 
-constexpr gpio_num_t MOTOR_DIRECTION = GPIO_NUM_19; // DMX-Shield DATA1 - shifted to 5V - [Pin 13]
-//TODO: Check PWM Pin and if we can take the signal directly from esp32. Avoid the level shifter.
-constexpr gpio_num_t MOTOR_STEP = GPIO_NUM_18;     //TODO: see above. DMX-Shield DATA0 - shifted to 5V - [Pin 12]
-constexpr gpio_num_t MOTOR_RUNNING = GPIO_NUM_5;    // DMX-Shield CLOCK - shifted to 5V - [Pin 7]
-// TODO: New feature: analog output (PWM) to set the speed ramp via webserver.
+// TODO: Feature: PWM out to set the speed ramp via webserver.
 
-
-
-/*
-// Stepper Motor Driver - TB6600 (common cathode wiring)
-constexpr gpio_num_t MOTOR_ENABLE = GPIO_NUM_27;    // DMX-Shield DATA2 - shifted to 5V - [Pin 6]
-constexpr gpio_num_t MOTOR_DIRECTION = GPIO_NUM_19; // DMX-Shield DATA1 - shifted to 5V - [Pin 13]
-constexpr gpio_num_t MOTOR_STEP = GPIO_NUM_18;      // DMX-Shield DATA0 - shifted to 5V - [Pin 12]
-*/
 
 // Push Buttons
-constexpr gpio_num_t BUTTON_AP = GPIO_NUM_25;
-constexpr gpio_num_t BUTTON_STOP = GPIO_NUM_26;
-
-/***************************************************
- * NOT IMPLEMENTED YET - CHECK PINS (!!!) BEFORE USE
-
-// Display - SSD1306 128x32px I2C OLED Display
-constexpr uint8_t DISPLAY_SDA = 21; //TODO: used by DMX Shield -> setup a different i2c bus. Stop the default?
-constexpr uint8_t DISPLAY_SCL = 22;
-
-// ?? Encoder
-constexpr uint8_t ENCODER_A = 12;
-constexpr uint8_t ENCODER_B = 13;
-*/
+// constexpr gpio_num_t BUTTON_AP = GPIO_NUM_25;
+// constexpr gpio_num_t BUTTON_STOP = GPIO_NUM_26;
+// TODO: Feature: Add buttons to stop the motor and turn the access point on/off
 
 #endif // MY_PIN_CONFIG_H
